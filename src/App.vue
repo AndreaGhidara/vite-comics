bbcf0c32090fbf32f55dc35fb061aac34d69542e
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-05-19 14:56:14 +0200

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import AppNavbar from './components/AppNavbar.vue';
+import AppContent from './components/AppContent.vue';
+import AppInfo from './components/AppInfo.vue'
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppNavbar,
+    AppContent,
+    AppInfo,
+    AppFooter,
+  },
+  data() {
+    return {
+      variabile: 42
+    }
+  },
+  methods: {
+    metodo() {
+      
+    }
+  },
+  mounted() {
+    
+  }
+}
+
+</script>
+
+<template>
+  <button class="m-5 p-1 btn btn-primary">ClickMe</button>
+</template>
+
+<style scoped>
+
+</style>
